94b3657e8a038c45ad5c8562ebd0c37b065ce300
Adam Tegtmeier <ategtmei@gmail.com>
2021-03-12 18:13:09 -0500

    This some how made it worse.

--- server/thing.py
+++ server/thing.py
@@ -12,10 +12,37 @@ if __name__ == '__main__':
 
 import logging
 import sys
+import os
+
+os.makedirs("log")
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(funcName)s:%(lineno)d %(message)s')
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(stream = sys.stdout)
-logger.addHander(handler)
+#handler = logging.StreamHandler(stream = sys.stdout)
+
+# Log errors to a file
+file_handler = logging.FileHandler("log/main.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+# Just so the console can see what is going on in stdout/stderr
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHander(file_handler)
+logger.addHander(stream_handler)
+
+logger.debug("Debug log message")
+logger.info("Info log message")
+logger.warn("Warn log message")
+
+# with open("log/main.log", "r") as f:
+#     print(r.read())
+
+#logger.addHander(handler)
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
