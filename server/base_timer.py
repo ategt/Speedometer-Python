16285a5806cc0559f5c58de6af32a22880fad300
Adam Tegtmeier <ategtmei@gmail.com>
2021-03-15 14:56:50 -0400

    Thought that I made the base timer class better/simpler without changing anything, but now a test is failing.

--- server/base_timer.py
+++ server/base_timer.py
@@ -12,62 +12,25 @@ def main():
 
   sio.connect("ws://127.0.0.1:5000/")
 
+  schedule = [{"activity":"Warmup",    "interval": 2*60},
+              {"activity":"Sprint!",   "interval": 30},
+              {"activity":"Recover",   "interval": 3*60},
+              {"activity":"Sprint!",   "interval": 30},
+              {"activity":"Recover",   "interval": 3*60},
+              {"activity":"Sprint!",   "interval": 30},
+              {"activity":"Recover",   "interval": 3*60},
+              {"activity":"Sprint!",   "interval": 30},
+              {"activity":"Recover",   "interval": 3*60},
+              {"activity":"Sprint!",   "interval": 30},
+              {"activity":"Cool Down", "interval": 3*60}]
+
   try:
-    for i in range(2*60, 0, -1):
-      print(f"\r Warmup - \t{i}    ", end="")
-      broadcast_update(sio, {"activity":"Warmup", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(30, 0, -1):
-      print(f"\r Sprint! - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Sprint!", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(3*60, 0, -1):
-      print(f"\r Recover - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Recover", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(30, 0, -1):
-      print(f"\r Sprint! - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Sprint!", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(3*60, 0, -1):
-      print(f"\r Recover - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Recover", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(30, 0, -1):
-      print(f"\r Sprint! - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Sprint!", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(3*60, 0, -1):
-      print(f"\r Recover - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Recover", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(30, 0, -1):
-      print(f"\r Sprint! - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Sprint!", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(3*60, 0, -1):
-      print(f"\r Recover - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Recover", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(30, 0, -1):
-      print(f"\r Sprint! - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Sprint!", "timeRemaining":i})
-      sleep(1)
-      
-    for i in range(3*60, 0, -1):
-      print(f"\r Cool Down - \t{i}   ", end="")
-      broadcast_update(sio, {"activity":"Cool Down", "timeRemaining":i})
-      sleep(1)
-      
+    for segment in schedule:
+      for i in range(segment['interval'], 0, -1):
+        print(f"\r {segment['activity']} - \t{i}    ", end="")
+        broadcast_update(sio, {"activity":segment['activity'], "timeRemaining":i})
+        sleep(0.1)
+
     print("\rDone!                                      ")
     broadcast_update(sio, {"activity":"Done!", "timeRemaining":0})
   finally:
