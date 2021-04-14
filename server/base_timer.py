1817ee05155fd9ffb54b91e502439985f3d3be75
Adam Tegtmeier <ategtmei@gmail.com>
2021-04-14 15:45:24 -0400

    Base timer is now setup to take arguments.

--- server/base_timer.py
+++ server/base_timer.py
@@ -1,5 +1,9 @@
 from time import sleep
 import socketio
+import pickle
+import base64
+import json
+import sys
 
 def broadcast_update(sio, result):
     try:
@@ -7,10 +11,27 @@ def broadcast_update(sio, result):
     except socketio.exceptions.BadNamespaceError as ex:
         pass
 
-def main():
-  sio = socketio.Client()
+def parse_args(system):
+  result = dict()
 
-  sio.connect("ws://127.0.0.1:5000/")
+  if len(system.argv) > 1:
+    result = pickle.loads(base64.decodebytes(bytes(system.argv[1], "utf-8")))
+
+  return result
+
+def serialize_args(uri, schedule):
+  " Build an argument string for use with parse_args. "
+  return base64.encodebytes(pickle.dumps({"uri":uri,"schedule":schedule})).decode().strip()
+
+def main():
+  """
+    When run from console, script accepts one argument.  Argument should be base64 
+    pickled dict of configuration arguments for run function.  Any omited dict key 
+    will result in the default value being used.
+
+    Script may be run without arguments, in which case, defaults will be used.
+  """
+  uri = "ws://127.0.0.1:5000/"
 
   schedule = [{"activity":"Warmup",    "interval": 2*60},
               {"activity":"Sprint!",   "interval": 30},
@@ -24,11 +45,19 @@ def main():
               {"activity":"Sprint!",   "interval": 30},
               {"activity":"Cool Down", "interval": 3*60}]
 
+  arguments = {"uri": uri, "schedule": schedule, **parse_args(sys)}
+  run(**arguments)
+
+def run(uri, schedule):
+  sio = socketio.Client()
+
+  sio.connect(uri)
+
   try:
     for segment in schedule:
       for i in range(segment['interval'], 0, -1):
         print(f"\r {segment['activity']} - \t{i}    ", end="")
-        broadcast_update(sio, {"activity":segment['activity'], "timeRemaining":i})
+        broadcast_update(sio, {**segment, "activity":segment['activity'], "timeRemaining":i})
         sleep(1)
 
     print("\rDone!                                      ")
---
+++ server/test_base_timer.py
@@ -0,0 +1,48 @@
+import unittest
+
+from time import sleep
+import base_timer
+
+class FakeSys:
+    def __init__(self, input):
+        self.argv = ["python.exe", input]
+
+class TestBaseTimer(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_argumentParser(self):
+        pickled_string = "gAN9cQAoWAMAAAB1cmlxAVgUAAAAd3M6Ly8xMjcuMC4wLjE6NTAwMC9xAlgIAAAAc2NoZWR1bGVx\nA11xBCh9cQUoWAgAAABhY3Rpdml0eXEGWAYAAABXYXJtdXBxB1gIAAAAaW50ZXJ2YWxxCEt4dX1x\nCShoBlgHAAAAU3ByaW50IXEKaAhLHnV9cQsoaAZYBwAAAFJlY292ZXJxDGgIS7R1fXENKGgGaApo\nCEsedX1xDihoBmgMaAhLtHV9cQ8oaAZoCmgISx51fXEQKGgGaAxoCEu0dX1xEShoBmgKaAhLHnV9\ncRIoaAZoDGgIS7R1fXETKGgGaApoCEsedX1xFChoBlgJAAAAQ29vbCBEb3ducRVoCEu0dWV1Lg=="
+
+        fakeSys = FakeSys(pickled_string)
+
+        result = base_timer.parse_args(fakeSys)
+
+        self.assertTrue('uri' in result.keys())
+        self.assertTrue('schedule' in result.keys())
+
+        self.assertEqual(type(result['schedule']), list)
+
+    def test_argumentSerializer(self):
+        uri = "ws://127.0.0.1:5000/"
+
+        schedule = [{"activity":"Warmup",    "interval": 2*60},
+                    {"activity":"Sprint!",   "interval": 30},
+                    {"activity":"Cool Down", "interval": 3*60}]
+
+        argument_string = base_timer.serialize_args(uri, schedule)
+
+        fakeSys = FakeSys(argument_string)
+
+        result = base_timer.parse_args(fakeSys)
+
+        self.assertTrue('uri' in result.keys())
+        self.assertTrue('schedule' in result.keys())
+
+        self.assertEqual(type(result['schedule']), list)
+
+if __name__ == '__main__':
+    unittest.main()
