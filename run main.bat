0f08b9f7d96bfbff4f300b4ddcab4ff952a0a466
Adam Tegtmeier <ategtmei@gmail.com>
2021-04-17 21:31:08 -0400

    Minor tweak to the main batch file so that the server will run with schedules.
    While I am at it, the package.json file seems to use the old version of parcel and no mocha or sinon.  And I may want vue-auth in there for the future.
    
    My notes still have look into flask endpoint/controller/resource files and python arduino uploading, neither of which I have touched yet.

--- run main.bat
+++ run main.bat
@@ -2,6 +2,7 @@ CD /D %~dp0
 
 @SET LOG_FILE_PATH=./data/speed-log.txt
 @SET REPORT_FILE_PATH=./data/reports.json
+@SET SCHEDULE_FILE_PATH=./data/schedules.json
 
 start "" "cmd.exe" "/c" "python.exe" "./server/app.py"
 start "" "cmd.exe" "/c" "python.exe" "./server/serial_recorder.py" "true"
