778376e66aed4fcdc83ded5218df5c3f3fa464eb
Adam Tegtmeier <ategtmei@gmail.com>
2021-04-20 14:38:39 -0400

    Report text details are loading imparatively.

--- client/src/components/Summary.vue
+++ client/src/components/Summary.vue
@@ -1,12 +1,56 @@
 <template>
-	<div id="summary" class="summary"></div>
+	<div id="summary" class="summary">
+		<div  v-if="report">
+			<div class="summary-grid">
+				<div class="summary-grid-item">ID</div>
+				<div class="summary-grid-item">{{report.id}}</div>
+				<div class="summary-grid-item">Top Speed</div>
+				<div class="summary-grid-item">{{report.topSpeed}}</div>
+				<div class="summary-grid-item">Avg. Sprint Speed</div>
+				<div class="summary-grid-item">{{ avgSprintSpeed }}</div>
+				<div class="summary-grid-item">Avg. Sprint Length</div>
+				<div class="summary-grid-item">{{ avgSprintLength }}</div>
+				<div class="summary-grid-item">Cool Down</div>
+				<div class="summary-grid-item">{{ coolDown }}</div>
+				<div class="summary-grid-item">Date</div>
+				<div class="summary-grid-item">{{display_string}}</div>
+				<div class="summary-grid-item">Faulty Reading Count</div>
+				<div class="summary-grid-item">{{report.faultyReadingCount}}</div>
+				<div class="summary-grid-item">Workout Length</div>
+				<div class="summary-grid-item">{{ totalLengthString }}</div>
+				<div class="summary-grid-item">Sprint Count</div>
+				<div class="summary-grid-item">{{report.sprintCount}}</div>
+				<div class="summary-grid-item">Start Time</div>
+				<div class="summary-grid-item">{{startTime.toLocaleTimeString()}}</div>
+				<div class="summary-grid-item">Stop Time</div>
+				<div class="summary-grid-item">{{stopTime.toLocaleTimeString()}}</div>
+			</div>
+			<textarea class="comment-textarea" v-model="report.remarks" v-on:blur="remarksUpdated"></textarea>
+		</div>
+		<div class="error-report" v-else-if="errors">
+			There was a problem loading the Report.
+		</div>
+		<div class="loading-report" v-else-if="loading">
+			Loading Report Details...
+		</div>
+		<div class="other-report" v-else>
+			No Report Details Found
+		</div>
+	</div>
 </template>
 <script>
-import { runOnLoad, load_summary, load_peak, top_speed } from '../src/summary';
+import { load_summary, load_peak, updateRemarks, top_speed } from '../src/summary';
 import { getReport, getReadings } from '../src/reports';
+import { months } from '../src/constants';
 import { detectPeaks } from '../src/peak-detection';
 import PeaksReadout from './PeaksReadout.vue';
 import axios from "axios";
+
+Number.prototype.pad = function (size) {
+	let s = String(this);
+	while ( s.length < ( size || 2)) { s = "0" + s;}
+	return s;
+}
 
 export default {
   name: 'Summary',
@@ -16,13 +60,52 @@ export default {
   data () {
   	return {
   		peaks: new Array(),
+  		report: null,
+  		loading: true,
+  		errors: new Array(),
+  	}
+  },
+  computed: {
+  	//const startTime = new Date(parseInt(report.startTime)*1000);
+	//const stopTime = new Date(parseInt(report.stopTime)*1000);
+	//const date = new Date(parseInt(report.date));
+
+	//const display_string = `${months[startTime.getMonth()]} ${startTime.getDate()}, ${startTime.getFullYear()}`;
+
+  	startTime: function () {
+  		return new Date(parseInt(this.report.startTime)*1000);
+  	},
+  	stopTime: function () {
+  		return new Date(parseInt(this.report.stopTime)*1000);
+  	},
+  	date: function () {
+  		return new Date(parseInt(this.report.date));
+  	},
+  	display_string: function () {
+  		return `${months[this.startTime.getMonth()]} ${this.startTime.getDate()}, ${this.startTime.getFullYear()}`;
+  	},
+  	avgSprintSpeed: function () {
+  		return Math.round(this.report.averageSpeedDuringSprint * 100)/100;
+  	},
+  	avgSprintLength: function () {
+  		return Math.round(this.report.avgSprintLength * 100)/100;
+  	},
+  	coolDown: function () {
+  		return `${Math.floor(this.report.cooldownTime/60)}:${(this.report.cooldownTime%60).pad(2)} (${this.report.cooldownTime})`;
+  	},
+  	totalLengthString: function () {
+  		return `${Math.floor(this.report.lengthOfWorkout/60)}:${(this.report.lengthOfWorkout%60).pad(2)} (${this.report.lengthOfWorkout})`;
   	}
   },
   methods: {
+  	remarksUpdated: function (event) {
+		updateRemarks(this.report.id, this.report.remarks);
+  	},
   	populate: function (report) {
-  		const empty_element = document.createElement("div");
-  		empty_element.id = "peaks-readout";
-  		load_summary(report, empty_element);
+  		this.report = report;
+  		//const empty_element = document.createElement("div");
+  		//empty_element.id = "peaks-readout";
+  		//load_summary(report, empty_element);
   		this.getPeaks(report.startTime, report.stopTime);
   	},
   	getPeaks: function (start, stop) {
@@ -40,6 +123,9 @@ export default {
 		const peaks = readings.map((itm, idx) => ({idx:idx, ...itm})).filter((itm, idx) => peakSet.has(idx));
 		this.peaks = peaks;
 		//const peaks_element = load_peak(peaks);
+  	},
+  	loadingDone: function () {
+  		this.loading = false;
   	}
   },
   created () {
@@ -57,7 +143,7 @@ export default {
 	
 	const reportId = parseInt(route.params.id);
 
-	getReport(reportId).then(this.populate)
+	getReport(reportId).then(this.populate).finally(this.loadingDone);
 
 	// axios.get("http://127.0.0.1:5000/readings", {params:params}).then(function (response) {
 	 //  	const peaksIndexes = detectPeaks(response.data.result.filter(d => d[0] < top_speed), d => d[0],
