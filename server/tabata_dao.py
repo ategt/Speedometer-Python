e7e255ad3a810553846544237f3aaea3480c29c2
Adam Tegtmeier <ategtmei@gmail.com>
2021-04-21 00:57:57 -0400

    Might have just made the tabata dao better.

--- server/tabata_dao.py
+++ server/tabata_dao.py
@@ -3,32 +3,59 @@ import json
 import os
 
 class TabataDao(object):
-	"""Record Actions directed at timer"""
-	def __init__(self, path = None):
-		super(TabataDao, self).__init__()
-		self.sqliteFile = os.path.join(os.getcwd(), 'data', 'database.sqlite3') if path is None else path
-		self._setupDb()
+    """Record Actions directed at timer"""
+    @staticmethod
+    def _dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
 
-	def _setupDb(self):
-	    self.conn = sqlite3.connect(self.sqliteFile)	    
-	    c = self.conn.cursor()
-	    c.execute('CREATE TABLE IF NOT EXISTS recorder_directives (action TEXT, time TIMESTAMP, pytime INTEGER)')
+        return d
 
-	    def dict_factory(cursor, row):
-		    d = {}
-		    for idx, col in enumerate(cursor.description):
-		        d[col[0]] = row[idx]
+    # def __init__(self, **kwargs):
+    #     super(DatabaseConnection, self).__init__()
+    #     self.dbPath = kwargs.get('dbPath', os.path.join(os.getcwd(), 'database.sqlite3'))
+    #     self._dict_factory = kwargs.get('dictFactory', DatabaseConnection._dict_factory)
 
-		    return d
+    def __init__(self, path = None):
+        super(TabataDao, self).__init__()
+        self.sqliteFile = os.path.join(os.getcwd(), 'data', 'database.sqlite3') if path is None else path
+        # self._setupDb()
 
-	    self.conn.row_factory = dict_factory
+    def _setupDb(self):
+        self.conn = sqlite3.connect(self.sqliteFile)        
+        c = self.conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS recorder_directives (action TEXT, time TIMESTAMP, pytime INTEGER)')
 
-	def recordDirective(self, action, pytime):
-	    _thiscursor = self.conn.cursor().execute('INSERT INTO recorder_directives (action, time, pytime) VALUES( ?, DATETIME("NOW"), ?)', (action, pytime))
-	    self.conn.commit()
+        def dict_factory(cursor, row):
+            d = {}
+            for idx, col in enumerate(cursor.description):
+                d[col[0]] = row[idx]
 
-	def close(self):
-		self.conn.close()
+            return d
 
-	def getAll(self):		
-		return self.conn.cursor().execute('SELECT * FROM recorder_directives').fetchall()
+        self.conn.row_factory = dict_factory
+
+    def __enter__(self):
+        self._setupDb()
+        # self.dbConnection = sqlite3.connect(self.dbPath)
+        # self.dbConnection.row_factory = self._dict_factory
+
+        return self
+
+    def __exit__(self, exec_type, exec_val, exec_tb):
+        self.close()
+                
+    def recordDirective(self, action, pytime):
+        _thiscursor = self.conn.cursor().execute('INSERT INTO recorder_directives (action, time, pytime) VALUES( ?, DATETIME("NOW"), ?)', (action, pytime))
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+
+    def getAll(self):
+        print(dir(self))
+        return self.conn.cursor().execute('SELECT * FROM recorder_directives').fetchall()
+
+# class DatabaseConnection(object):    
+#     """docstring for DatabaseConnection"""
--- server/test_tabata_dao.py
+++ server/test_tabata_dao.py
@@ -14,19 +14,19 @@ class TestTabataDao(unittest.TestCase):
         self.tempFile = uuid.uuid4().hex
 
     def tearDown(self):
-        self.tabataDao.close()
         os.remove(self.tempFile)
 
     def test_actionRecorder(self):
-        self.tabataDao = TabataDao(self.tempFile)
-        self.tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
-        self.tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
-        self.tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
+        with TabataDao(self.tempFile) as tabataDao:
+            tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
+            tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
+            tabataDao.recordDirective(uuid.uuid4().hex, random.randint(-2**31, 2**31))
 
-        tabataDao2 = TabataDao(self.tempFile)
-        everything = tabataDao2.getAll()
-        self.assertTrue(len(everything) > 1)
-        self.assertEqual(len(everything),  3)
+        with TabataDao(self.tempFile) as tabataDao2:
+            everything = tabataDao2.getAll()
+
+            self.assertTrue(len(everything) > 1)
+            self.assertEqual(len(everything),  3)
 
 if __name__ == '__main__':
     unittest.main()
