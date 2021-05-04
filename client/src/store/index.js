d5b05ec64a883a3fade0217cc0a4924153b39fe1
Adam Tegtmeier <ategtmei@gmail.com>
2021-05-04 10:14:47 -0400

    Minor update.  Added schedule module to vuex config.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import reports from './modules/reports';
 import readings from './modules/readings';
+import schedules from './modules/schedules';
 //import createLogger from '../../../src/plugins/logger'
 
 Vue.use(Vuex)
@@ -12,6 +13,7 @@ export default new Vuex.Store({
   modules: {
     reports,
     readings,
+    schedules,
   },
   strict: debug,
   plugins: [] //debug ? [createLogger()] : []
