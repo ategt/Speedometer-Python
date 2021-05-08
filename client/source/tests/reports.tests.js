5da69e987ad3a997d0a0a5ecd33ba386167fe50e
Adam Tegtmeier <ategtmei@gmail.com>
2021-05-08 01:32:53 -0400

    Reports tests seems to be passing now.  About to kick some tires.

--- client/source/tests/reports.tests.js
+++ client/source/tests/reports.tests.js
@@ -13,6 +13,20 @@ import { REALTIME_SPEED_DATA, RECORED_SPEED_DATA, DEFAULT_SCHEDULE, REALTIME_SLO
 const expect = chai.expect;
 
 describe('Report Shared Script Test', function () {
+
+  const tempDate = new Date();
+  tempDate.setFullYear( 2015 );
+
+  const beginingOfService = +tempDate;
+
+  tempDate.setFullYear( 2040 );
+
+  const endOfService = +tempDate;
+
+  const serviceSpan = endOfService - beginingOfService;
+
+  const updateCorrector = reading => Object.assign({}, reading, { recieved: Math.floor((Math.random() * serviceSpan) + beginingOfService) });
+
   describe('sort recorded readings', function () {
     it('handle one report', () => {
       const result = sort_readings([RECORED_SPEED_DATA[0]]);
@@ -55,27 +69,65 @@ describe('Report Shared Script Test', function () {
     });
 
     const mapToRecord = item => Object.assign({}, item, {timestamp: convertTimestampToServer(item.recieved)});
+    const corrected_RealtimeSpeedData = REALTIME_SPEED_DATA.map(updateCorrector).map(mapToRecord)
 
     it('sequence reports', () => {
-      const timecodes = REALTIME_SPEED_DATA.map(item => convertTimestampToServer(item.recieved));
+      const timecodes = corrected_RealtimeSpeedData.map(item => convertTimestampToServer(item.recieved));
 
       const start_time = Math.min(...timecodes);
       const stop_time  = Math.max(...timecodes);
 
 
-      const result = sort_readings(REALTIME_SPEED_DATA.map(mapToRecord));
+      const result = sort_readings(corrected_RealtimeSpeedData);
 
       expect(parseFloat(result[0].timestamp)).equal(start_time);
       expect(parseFloat(result[result.length - 1].timestamp)).equal(stop_time);
     });
 
     it('sequence reversed reports', () => {
-      const timecodes = REALTIME_SPEED_DATA.map(item => convertTimestampToServer(item.recieved));
+      const timecodes = corrected_RealtimeSpeedData.map(item => convertTimestampToServer(item.recieved));
 
       const start_time = Math.min(...timecodes);
       const stop_time  = Math.max(...timecodes);
 
-      const reversedRecoredSpeedData = Array.from(REALTIME_SPEED_DATA).reverse();
+      const reversedRecoredSpeedData = Array.from(corrected_RealtimeSpeedData).reverse();
+
+      const result = sort_readings(corrected_RealtimeSpeedData);
+
+      expect(parseFloat(result[0].timestamp)).equal(start_time);
+      expect(parseFloat(result[result.length - 1].timestamp)).equal(stop_time);
+    });
+  });
+
+  describe('sort slow realtime readings', function () {
+    it('handle one report', () => {
+      const result = sort_readings([REALTIME_SLOW_SPEED_DATA[0]]);
+
+      expect(result[0].currentRevsPerMin).equal(REALTIME_SLOW_SPEED_DATA[0].currentRevsPerMin);
+    });
+
+    const mapToRecord = item => Object.assign({}, item, {timestamp: convertTimestampToServer(item.recieved)});
+
+    it('sequence reports', () => {
+      const timecodes = REALTIME_SLOW_SPEED_DATA.map(item => convertTimestampToServer(item.recieved));
+
+      const start_time = Math.min(...timecodes);
+      const stop_time  = Math.max(...timecodes);
+
+
+      const result = sort_readings(REALTIME_SLOW_SPEED_DATA.map(mapToRecord));
+
+      expect(parseFloat(result[0].timestamp)).equal(start_time);
+      expect(parseFloat(result[result.length - 1].timestamp)).equal(stop_time);
+    });
+
+    it('sequence reversed reports', () => {
+      const timecodes = REALTIME_SLOW_SPEED_DATA.map(item => convertTimestampToServer(item.recieved));
+
+      const start_time = Math.min(...timecodes);
+      const stop_time  = Math.max(...timecodes);
+
+      const reversedRecoredSpeedData = Array.from(REALTIME_SLOW_SPEED_DATA).reverse();
 
       const result = sort_readings(reversedRecoredSpeedData.map(mapToRecord));
 
@@ -110,9 +162,10 @@ describe('Report Shared Script Test', function () {
     });
 
     it('using realtime data', () => {
-      const report = prepare_report(REALTIME_SPEED_DATA, DEFAULT_SCHEDULE);
+      const correction_for_code_change = REALTIME_SPEED_DATA.map(updateCorrector);
+      const report = prepare_report(correction_for_code_change, DEFAULT_SCHEDULE);
 
-      const timecodes = REALTIME_SPEED_DATA.map(item => convertTimestampToServer(item.recieved));
+      const timecodes = correction_for_code_change.map(item => convertTimestampToServer(item.recieved));
 
       const start_time = Math.min(...timecodes);
       const stop_time  = Math.max(...timecodes);
@@ -120,8 +173,6 @@ describe('Report Shared Script Test', function () {
       equal(report.scheduleId, DEFAULT_SCHEDULE.id);
       equal(report.scheduleName, DEFAULT_SCHEDULE.name);
 
-      //equal(Number.isFinite(report.averageSpeedDuringSprint), true);
-      //equal(Number.isFinite(report.avgSprintLength),          true);
       equal(Number.isFinite(report.startTime), true);
       equal(Number.isFinite(report.stopTime),  true);
 
@@ -146,8 +197,6 @@ describe('Report Shared Script Test', function () {
       equal(report.scheduleId, DEFAULT_SCHEDULE.id);
       equal(report.scheduleName, DEFAULT_SCHEDULE.name);
 
-      //equal(Number.isFinite(report.averageSpeedDuringSprint), true);
-      //equal(Number.isFinite(report.avgSprintLength),          true);
       equal(Number.isFinite(report.startTime), true);
       equal(Number.isFinite(report.stopTime),  true);
 
